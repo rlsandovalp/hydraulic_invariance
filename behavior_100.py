247fba8af70a16472ec3e3d4bb34bc030428ebab
Leonardo <rlsandovalp@unal.edu.co>
2020-09-11 20:08:32 +0200

    añadit behavior100

---
+++ behavior_100.py
@@ -0,0 +1,16 @@
+import arcpy
+from arcpy import env
+
+# Variables of the files and folders
+split2 = arcpy.env.workspace+"\\shapes\\split2.shp"
+symbology100 = arcpy.env.workspace+"\\..\\Lombardy\\General\\layers\\symbology100.lyr"
+
+# Load the shapefile
+mxd = arcpy.mapping.MapDocument("CURRENT")  
+df = arcpy.mapping.ListDataFrames(mxd)[0]  
+arcpy.MakeFeatureLayer_management(split2, "split2")  
+
+# Apply the symbology and show the shapefile in the canvas
+arcpy.ApplySymbologyFromLayer_management(in_layer="split2", in_symbology_layer=symbology100)
+addLayer = arcpy.mapping.Layer("split2") 
+arcpy.mapping.AddLayer(df,addLayer) 

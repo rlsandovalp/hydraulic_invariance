9d24f9546b00c716193f096f33f8a5bc60410ba4
Leonardo Sandoval <rlsandovalp@unal.edu.co>
2024-04-06 16:34:04 -0700

    missing requirement

--- 00_🌧️_hyetographs.py
+++ 00_🌧️_hyetographs.py
@@ -2,8 +2,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
-import plotly.figure_factory as ff
-import plotly.express as px
 import plotly.graph_objects as go
 
 

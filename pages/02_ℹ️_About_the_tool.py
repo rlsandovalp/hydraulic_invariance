dd565289fe6019c571a2f7433bbb412e211b64dd
Leonardo Sandoval <rlsandovalp@unal.edu.co>
2024-04-07 09:21:48 -0700

    aaa

---
+++ pages/02_ℹ️_About_the_tool.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="About this tool",
+    page_icon="ℹ️",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+st.header('About this tool ℹ️📄')
+
+r'''
+This tool was developed by [Leonardo Sandoval](https://www.linkedin.com/in/rafael-leonardo-sandoval-pab%C3%B3n-6a04b89b/) to estimate hyetographs for hydrological studies, following the LSPP (Linee Segnalatrici di Possibilità Pluviometrica) methodology typically employed in the Lombardy region of Italy.
+
+The tool request information of the effective parameters of the LSPP method for a given area as well as the total duration of the rain, the time intervals for which you want to recover the precipitation, the position of the peak $r$, and the return period. Then it calculates the rain depth for an uniform hyetogram and a Chicago hyetogram, plotting the results, and allow the user to download the hyetogram data than can be used in SWMM (or any other software) for further analysis.
+'''
